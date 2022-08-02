84d954a240334f8e6f4d88cc007e77156c5610ab
MatheusEduardo <matheus.eduardo@versatecnologia.com.br>
2022-08-02 14:53:06 -0300

    Implementações

--- index.php
+++ index.php
@@ -2,17 +2,17 @@
 
 require_once("vendor/autoload.php");
 
-$app = new \Slim\Slim();
+use \Slim\Slim;
+use \Hcode\Page;
+
+$app = new Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
-    
-	// echo "OK";
-	$sql = new \Hcode\DB\Sql();
-	$results = $sql->select("SELECT * FROM tb_users");
+	$page = new Page();
+	$page->setTpl('index');
 
-	echo json_encode($results);
 });
 
 $app->run();
